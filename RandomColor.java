6beded0522037e6992992d04f1acfc0e052e10bb
Costnertk <58087296+Costnertk@users.noreply.github.com>
2020-07-19 18:04:17 -0400

    new random color gui

---
+++ RandomColor.java
@@ -0,0 +1,51 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class RandomColor implements ActionListener {
+    JFrame frame;
+    JPanel panel;
+    JButton button;
+
+    public RandomColor() {
+        frame = new JFrame();
+        panel = new JPanel();
+        button = new JButton("Start/Stop");
+
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+        panel.add(button);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setTitle("Flashing Lights!!!!!");
+        frame.pack();
+
+        button.addActionListener(this);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new RandomColor();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        while(true){
+            if(button.isEnabled()){
+                int RValue = (int) (Math.random() * 255);
+                int GValue = (int) (Math.random() * 255);
+                int BValue = (int) (Math.random() * 255);
+                panel.setBackground(new Color(RValue, GValue, BValue));
+            }
+            else if(!button.isEnabled()){
+                break;
+            }
+        }
+    }
+}
