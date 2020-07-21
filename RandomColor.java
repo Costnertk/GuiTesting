2fb4189dcfbfa077d094a64f8c5ac5f387d0c3bb
Costner Magill <costnerxman360@gmail.com>
2020-07-20 20:50:43 -0400

    tried to get time to work

--- RandomColor.java
+++ RandomColor.java
@@ -2,7 +2,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalTime;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
